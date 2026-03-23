a629ae6772f7600e1da763a66ea9ce254f6bec7b
jamesfowler-dev <james_fowler21@hotmail.com>
2026-03-23 09:56:30 +0000

    add heading to app.jsx, input bar component, add and reset buttons.

---
+++ src/components/InputBar.jsx
@@ -0,0 +1,16 @@
+const InputBar = () => {
+
+    return (
+    <div>
+        <label></label>
+        <input 
+            type="text" 
+            placeholder="Add your task here...">
+
+        </input>
+        <button>Add</button>
+    </div>
+    )
+}
+
+export default InputBar
---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,5 @@
+const ToDoItem = () => {
+
+}
+
+export default ToDoItem
