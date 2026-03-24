fa3ecc8b04c9a01cb968a68515188e2edc8c5280
jamesfowler-dev <james_fowler21@hotmail.com>
2026-03-24 13:55:16 +0000

    package.json and vite.change for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 
 import InputBar from "./components/InputBar.jsx";
-import ToDoItem from "./components/ToDoItem.jsx";
 import ToDoList from "./components/ToDoList.jsx";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
